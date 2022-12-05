12c0bd12b12d81ee5dded7514a42a1953aeea722
Baer Lanfried <blanfried@yahoo.com>
2022-12-05 13:58:36 +0000

    fixed all errors and now functioning properly

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,105 +1,105 @@
 let resumeData = {
-      "name": "Baer Lanfried",
-      "role": "Philosopher and Full-Stack Developer",
-      "linkedinId":"https://www.linkedin.com/in/baer-lanfried-488ab61aa/",
-      "roleDescription": "I enjoy coding and working out, reading, or playing board games in my free time",
-      "socialLinks":[
-          {
-            "name":"linkedin",
-            "url":"https://www.linkedin.com/in/baer-lanfried-488ab61aa/",
-            "className":"fa fa-linkedin"
-          },
-          {
-            "name":"github",
-            "url":"http://github.com/blanfried",
-            "className":"fa fa-github"
-          }
-        ],
-      "aboutme":"I have just finished a MA in philosophy of religion and ethics in the UK at the University of Birmingham. During the MA I started a coding bootcamp and am now looking for full-stack web development or software development jobs.",
-      "address":"California",
-      "education":[
-        {
-          "UniversityName":"American University of Paris",
-          "specialization":"Philosophy",
-          "MonthOfPassing":"June",
-          "YearOfPassing":"2020",
-          "Achievements":"BA"
-        },
-        {
-          "UniversityName":"American University of Paris",
-          "specialization":"Psychology",
-          "MonthOfPassing":"June",
-          "YearOfPassing":"2020",
-          "Achievements":"BA"
-        },
-        {
-          "UniversityName":"University of Birmingham",
-          "specialization":"Philosophy of Religion and Ethics",
-          "MonthOfPassing":"December",
-          "YearOfPassing":"2022",
-          "Achievements":"MA"
-        }
-      ],
-      // "work":[
-      //   {
-      //     "CompanyName":"Some Company",
-      //     "specialization":"Some specialization",
-      //     "MonthOfLeaving":"Jan",
-      //     "YearOfLeaving":"2018",
-      //     "Achievements":"Some Achievements"
-      //   },
-      //   {
-      //     "CompanyName":"Some Company",
-      //     "specialization":"Some specialization",
-      //     "MonthOfLeaving":"Jan",
-      //     "YearOfLeaving":"2018",
-      //     "Achievements":"Some Achievements"
-      //   }
-      // ],
-      "skillsDescription":"Your skills here",
-      "skills":[
-        {
-          "skillname":"HTML"
-        },
-        {
-          "skillname":"CSS"
-        },
-        {
-          "skillname":"Reactjs"
-        }
-      ],
-      // "portfolio":[
-      //   {
-      //     "name":"project1",
-      //     "description":"mobileapp",
-      //     "imgurl":"images/portfolio/phone.jpg"
-      //   },
-      //   {
-      //     "name":"project2",
-      //     "description":"mobileapp",
-      //     "imgurl":"images/portfolio/project.jpg"
-      //   },
-      //   {
-      //     "name":"project3",
-      //     "description":"mobileapp",  
-      //     "imgurl":"images/portfolio/project2.png"
-      //   },
-      //   {
-      //     "name":"project4",
-      //     "description":"mobileapp",
-      //     "imgurl":"images/portfolio/phone.jpg"
-      //   }
-      // ],
-      // "testimonials":[
-      //   {
-      //     "description":"This is a sample testimonial",
-      //     "name":"Some technical guy"
-      //   },
-      //   {
-      //     "description":"This is a sample testimonial",
-      //     "name":"Some technical guy"
-      //   }
-      // ]
+  "imagebaseurl":"https://github.com/blanfried",
+  "name": "Baer Lanfried",
+  "role": "Philosopher and Full-Stack Developer",
+  "linkedinId":"https://www.linkedin.com/in/baer-lanfried-488ab61aa/",
+  "roleDescription": "I enjoy coding and working out, reading, or playing board games in my free time",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/baer-lanfried-488ab61aa/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"http://github.com/blanfried",
+        "className":"fa fa-github"
+      }
+    ],
+  "aboutme":"I have just finished a MA in philosophy of religion and ethics in the UK at the University of Birmingham. During the MA I started a coding bootcamp and am now looking for full-stack web development or software development jobs.",
+  "address":"California",
+  "education":[
+    {
+      "UniversityName":"American University of Paris",
+      "specialization":"Philosophy",
+      "MonthOfPassing":"June",
+      "YearOfPassing":"2020",
+      "Achievements":"BA"
+    },
+    {
+      "UniversityName":"American University of Paris",
+      "specialization":"Psychology",
+      "MonthOfPassing":"June",
+      "YearOfPassing":"2020",
+      "Achievements":"BA"
+    },
+    {
+      "UniversityName":"University of Birmingham",
+      "specialization":"Philosophy of Religion and Ethics",
+      "MonthOfPassing":"December",
+      "YearOfPassing":"2022",
+      "Achievements":"MA"
     }
-    
-    export default resumeData
+  ],
+  // "work":[
+  //   {
+  //     "CompanyName":"Some Company",
+  //     "specialization":"Some specialization",
+  //     "MonthOfLeaving":"Jan",
+  //     "YearOfLeaving":"2018",
+  //     "Achievements":"Some Achievements"
+  //   },
+  //   {
+  //     "CompanyName":"Some Company",
+  //     "specialization":"Some specialization",
+  //     "MonthOfLeaving":"Jan",
+  //     "YearOfLeaving":"2018",
+  //     "Achievements":"Some Achievements"
+  //   }
+  // ],
+  "skills":[
+    {
+      "skillname":"HTML"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  // "portfolio":[
+  //   {
+  //     "name":"project1",
+  //     "description":"mobileapp",
+  //     "imgurl":"images/portfolio/phone.jpg"
+  //   },
+  //   {
+  //     "name":"project2",
+  //     "description":"mobileapp",
+  //     "imgurl":"images/portfolio/project.jpg"
+  //   },
+  //   {
+  //     "name":"project3",
+  //     "description":"mobileapp",  
+  //     "imgurl":"images/portfolio/project2.png"
+  //   },
+  //   {
+  //     "name":"project4",
+  //     "description":"mobileapp",
+  //     "imgurl":"images/portfolio/phone.jpg"
+  //   }
+  // ],
+  // "testimonials":[
+  //   {
+  //     "description":"This is a sample testimonial",
+  //     "name":"Some technical guy"
+  //   },
+  //   {
+  //     "description":"This is a sample testimonial",
+  //     "name":"Some technical guy"
+  //   }
+  // ]
+}
+
+export default resumeData
