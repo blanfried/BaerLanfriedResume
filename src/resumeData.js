12bdccc21591f4733df161b5cb0bfe9b95c9ec59
Baer Lanfried <blanfried@yahoo.com>
2022-12-05 14:57:00 +0000

    added a project and now done

--- src/resumeData.js
+++ src/resumeData.js
@@ -72,34 +72,20 @@ let resumeData = {
     {
       "name":"SCA-Rental",
       "description":"Car Rental Agency https://github.com/blanfried/SCA-Rental",
+      "imgurl":"images/portfolio/project.jpg"
+    }
+  ],
+  
+  "testimonials":[
+    {
+      "description":"Just such a good guy",
+      "name":"Baer's friend"
+    },
+    {
+      "description":"Seriously cool dude",
+      "name":"Baer's other friend"
     }
   ]
-  //   {
-  //     "name":"project2",
-  //     "description":"mobileapp",
-  //     "imgurl":"images/portfolio/project.jpg"
-  //   },
-  //   {
-  //     "name":"project3",
-  //     "description":"mobileapp",  
-  //     "imgurl":"images/portfolio/project2.png"
-  //   },
-  //   {
-  //     "name":"project4",
-  //     "description":"mobileapp",
-  //     "imgurl":"images/portfolio/phone.jpg"
-  //   }
-  // ],
-  // "testimonials":[
-  //   {
-  //     "description":"This is a sample testimonial",
-  //     "name":"Some technical guy"
-  //   },
-  //   {
-  //     "description":"This is a sample testimonial",
-  //     "name":"Some technical guy"
-  //   }
-  // ]
 }
 
 export default resumeData
