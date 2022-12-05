8eb3004c72d62369f24b3cf8d63bdae6b4ed5369
Baer Lanfried <blanfried@yahoo.com>
2022-12-05 14:36:29 +0000

    Updates

---
+++ precache-manifest.2f0a9c5662839e98483f25dbd3d430ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a7c1a91a8d5efe12f12",
+    "url": "/blanfried_react_app/static/css/main.0b43131e.chunk.css"
+  },
+  {
+    "revision": "6a7c1a91a8d5efe12f12",
+    "url": "/blanfried_react_app/static/js/main.6a7c1a91.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/blanfried_react_app/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "5cab4d1497bd454d323a",
+    "url": "/blanfried_react_app/static/js/runtime~main.5cab4d14.js"
+  },
+  {
+    "revision": "31ed514ec27d0708d598475eef3dca31",
+    "url": "/blanfried_react_app/index.html"
+  }
+];
