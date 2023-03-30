16ae2d3ab596099d8d83b97437c99502fcbe1bb8
Baer Lanfried <blanfried@yahoo.com>
2023-03-30 17:27:45 -0400

    removed quotes symbol from testimonials because I added awards to testimonials section, therefore the quotes symbol no longer made sense

--- src/resumeData.js
+++ src/resumeData.js
@@ -60,39 +60,81 @@ let resumeData = {
   ],
   "work":[
     {
-      "CompanyName":"Mysterious Bookshop",
-      "specialization":"Part-time Bookseller",
-      "MonthOfLeaving":"Jan 2023 -",
-      "YearOfLeaving":"April 2023",
-      "Achievements":" Sold books, made recommendations, re-shelved/organized books in a presentable way, worked cash register and dealt with customers effectively and professionally."
+      "CompanyName":"Oso Adventurous",
+      "specialization":"Creator/Travel Vlogger",
+      "MonthOfLeaving":"August 2020 -",
+      "YearOfLeaving":"Present",
+      "Achievements":"Nearly 160k views and 30,000 hours watched on my YouTube videos (on and off this channel)."
     },
     {
       "CompanyName":"Mysterious Bookshop",
       "specialization":"Part-time Bookseller",
       "MonthOfLeaving":"Jan 2023 -",
       "YearOfLeaving":"April 2023",
-      "Achievements":" Sold books, made recommendations, re-shelved/organized books in a presentable way, worked cash register and dealt with customers effectively and professionally."
+      "Achievements":"Sold books, made recommendations, re-shelved/organized books in a presentable way, worked cash register and dealt with customers effectively and professionally."
     },
     {
-      "CompanyName":"Mysterious Bookshop",
-      "specialization":"Part-time Bookseller",
-      "MonthOfLeaving":"Jan 2023 -",
-      "YearOfLeaving":"April 2023",
-      "Achievements":" Sold books, made recommendations, re-shelved/organized books in a presentable way, worked cash register and dealt with customers effectively and professionally."
+      "CompanyName":"Freelance",
+      "specialization":"English Teacher",
+      "MonthOfLeaving":"August 2020 -",
+      "YearOfLeaving":"August 2022",
+      "Achievements":"Full-time private english teacher to adults and teenagers of all different levels in Biarritz, France. Part-time during my MA for one year. Also aided in writing, editing, and proofreading students’ professional assignments."
     },
     {
-      "CompanyName":"Mysterious Bookshop",
-      "specialization":"Part-time Bookseller",
-      "MonthOfLeaving":"Jan 2023 -",
-      "YearOfLeaving":"April 2023",
-      "Achievements":" Sold books, made recommendations, re-shelved/organized books in a presentable way, worked cash register and dealt with customers effectively and professionally."
+      "CompanyName":"The American University of Paris",
+      "specialization":"Writing Tutor",
+      "MonthOfLeaving":"September 2019 -",
+      "YearOfLeaving":"May 2020",
+      "Achievements":"Edited/proofread academic papers, provided feedback, and guided fellow students to better writing techniques."
     },
     {
-      "CompanyName":"Mysterious Bookshop",
-      "specialization":"Part-time Bookseller",
-      "MonthOfLeaving":"Jan 2023 -",
-      "YearOfLeaving":"April 2023",
-      "Achievements":" Sold books, made recommendations, re-shelved/organized books in a presentable way, worked cash register and dealt with customers effectively and professionally."
+      "CompanyName":"Paris Atlantic Magazine",
+      "specialization":"Editorial Assistant",
+      "MonthOfLeaving":"September 2019 -",
+      "YearOfLeaving":"May 2020",
+      "Achievements":"Edited/proofread articles, provided feedback, and published one of my short stories"
+    },
+    {
+      "CompanyName":"“Let’s Talk About God” Club and Radio Show",
+      "specialization":"President/Founder",
+      "MonthOfLeaving":"September 2019 -",
+      "YearOfLeaving":"May 2020",
+      "Achievements":"Host of radio show which broadcasted to all of Paris and the French Riviera. Organized and mediated oftentimes tense discussions surrounding deeply held beliefs in both club and show. Used charisma and creativity to keep conversation/debate lively and interesting."
+    },
+    {
+      "CompanyName":"The American University of Paris",
+      "specialization":"Student Advisor",
+      "MonthOfLeaving":"August 2019 -",
+      "YearOfLeaving":"December 2019",
+      "Achievements":"Advisor for incoming fall transfer students."
+    },
+    {
+      "CompanyName":"The American University of Paris",
+      "specialization":"Student Librarian",
+      "MonthOfLeaving":"September 2018 -",
+      "YearOfLeaving":"May 2019",
+      "Achievements":"Aided students and staff in research and finding books/materials. Organized library books/materials."
+    },
+    {
+      "CompanyName":"Maxwell-Baynes, Christie's International Real Estate",
+      "specialization":"Intern/Shadow",
+      "MonthOfLeaving":"May 2018 -",
+      "YearOfLeaving":"July 2018",
+      "Achievements":"I shadowed my boss and co-owner of the company, Michael Baynes, and learned how he dealt with clients and communicated effectively in order to sell chateaus and vineyards. Also aided in planning and communication between team and clients."
+    },
+    {
+      "CompanyName":"Cucina Enoteca",
+      "specialization":"Busser/Polisher",
+      "MonthOfLeaving":"April 2016 -",
+      "YearOfLeaving":"August 2016",
+      "Achievements":"Bussed and Polished at a high-end Italian Restaurant in Newport Beach, CA"
+    },
+    {
+      "CompanyName":"City of Newport Beach",
+      "specialization":"Lifeguard",
+      "MonthOfLeaving":"March 2014 -",
+      "YearOfLeaving":"September 2014",
+      "Achievements":"Beach lifeguard trainee and first year cadet for city of Newport Beach Fire Department Marine Operations Division. Pool Lifeguard for the remainder of that summer and for a bit into that school year."
     }
   ],
   
@@ -170,12 +212,16 @@ let resumeData = {
   ],
     "testimonials":[
     {
-      "description":"This is a sample testimonial",
-      "name":"Some technical guy"
+      "description":"\"Global Professional Skills Honour\"",
+      "name":"The American University of Paris"
     },
     {
-      "description":"This is a sample testimonial",
-      "name":"Some technical guy"
+      "description":"Provost's List (May 2020)",
+      "name":"The American University of Paris"
+    },
+    {
+      "description":"Provost's List (May 2019)",
+      "name":"The American University of Paris"
     }
   ]
 }
