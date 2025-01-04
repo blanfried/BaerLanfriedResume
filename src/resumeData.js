c5388c5ac92ed973c9c97f42ba7f5edad5f7b87c
“blanfried” <blanfried@yahoo.com>
2025-01-04 09:59:28 -0800

    updated about

--- src/resumeData.js
+++ src/resumeData.js
@@ -51,7 +51,7 @@ let resumeData = {
         "className":"fab fa-tiktok"
       }
     ],
-  "aboutme":"I grew up in Southern California but have spent the majority of my adult life living in France and the United Kingdom. These days, I live semi-nomadically but principally between California (with my family) and Wales (with my girlfriend). I'm building my business (TheBaerBones.com) and educating when and where I can. Professional languages: English | French | Spanish",
+  "aboutme":"I grew up in Southern California but have spent the majority of my adult life living in France and the United Kingdom. These days, I live principally between California (with my family) and Wales (with my girlfriend). I'm building my business (TheBaerBones.com) and educating when and where I can. Professional languages: English | French | Spanish",
   "address":"PO Box 580, Pauma Valley, CA 92061, USA",
   "education":[
     {
